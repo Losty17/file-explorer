f0f0c805d27465a2886b973edd22a319eb9b9b82
Vinícius Kappke <vinikappke@gmail.com>
2022-11-24 23:19:20 +0000

    Impl of a stack to handle fs path

--- main.c
+++ main.c
@@ -3,51 +3,205 @@
 #include <string.h>
 #include <stdbool.h>
 
+typedef struct stack_t
+{
+    char *data;
+    struct stack_t *next;
+} stack_t;
+
+/**
+ * @brief Tipos utilizados para representar os
+ * tipos de nodo da árvore.
+ */
 enum Types
 {
     _FILE,
     _FOLDER
 };
 
+/**
+ * Definição dos dados utilizados em um nodo da árvore.
+ *
+ * @param type Indica se é uma pasta ou um arquivo.
+ * @param name Nome do arquivo ou pasta.
+ * @param content Conteúdo do arquivo.
+ * @param can_read Permissão de leitura.
+ * @param can_write Permissão de escrita.
+ */
 typedef struct tree_data_t
 {
     enum Types type;
     char *name, *content;
+    bool can_read, can_write;
+    // bool readonly
 } tree_data_t;
 
-typedef struct tree_node_t 
+/**
+ * Definição de um nodo da árvore.
+ *
+ * @param data Dados do nodo.
+ * @param parent Nodo pai.
+ * @param child Primeiro filho do nodo.
+ * @param sibling Próximo nodo irmão.
+ * @param depth Profundidade do nodo.
+ */
+typedef struct tree_node_t
 {
     struct tree_data_t data;
     struct tree_node_t *parent, *child, *sibling;
     int depth;
 } tree_node_t;
 
+/**
+ * Definição do estado do terminal.
+ *
+ * @param curr_node Nodo atual.
+ * @param curr_path Caminho atual.
+ */
 typedef struct navigation_data_t
 {
-    char *path;
+    tree_node_t *curr_node, *root;
+    stack_t *stack_path;
 } navigation_data_t;
 
-char *get_new_path(char *path, int depth)
+// --- NODE UTILS ---
+/**
+ * @brief Cria um novo nodo.
+ *
+ * @param parent Nodo pai.
+ * @param name Nome do nodo.
+ * @param type Tipo do nodo.
+ */
+tree_node_t *tree_create_node(tree_node_t *parent, char *name, enum Types type, bool can_read, bool can_write);
+/**
+ * @brief Busca um nodo através de um caminho de file system (/pasta/pasta/arquivo).
+ *
+ * @param current Nodo para iniciar a busca.
+ * @param path Caminho para buscar.
+ */
+tree_node_t *tree_find(tree_node_t *current, char *path);
+
+// --- COMMANDS ---
+/**
+ * @brief Avança ou retorna o diretório atual.
+ *
+ * @param navigation_data provém as informações necessárias para alterar o estado do terminal.
+ * @param path Caminho a ser alterado.
+ */
+void cmd_change_directory(navigation_data_t **navigation_data, char *path);
+/**
+ * @brief Exibe os filhos do nodo informado.
+ *
+ * @param node Nodo a ser exibido.
+ */
+void cmd_list(tree_node_t *node);
+
+// --- FILE SYSTEM ---
+/**
+ * @brief Inicializa um nodo raiz com uma arquitetura de arquivos
+ * semelhante a um sistema operacional UNIX (Linux e derivados).
+ */
+tree_node_t *fs_init();
+tree_node_t *fs_create_folder(tree_node_t *parent, char *name);
+tree_node_t *fs_create_file(tree_node_t *parent, char *name, char *content, bool can_read, bool can_write);
+/**
+ * @brief Retorna o caminho da pasta atual.
+ * 
+ * @param navigation_data provém as informações necessárias para identificar a pasta atual.
+ */
+char *fs_get_current_path(navigation_data_t *navigation_data);
+
+// --- STACK ---
+stack_t *stack_init(char *data);
+stack_t *stack_pop(stack_t **stack);
+void stack_push(stack_t **stack, char *data);
+void stack_free(stack_t *stack);
+
+// --- MAIN FUNCTION ---
+int main()
 {
-    char *ptr = strtok(path, "/");
+    tree_node_t *root = fs_init();
 
-    depth++;
-    while (depth--)
-        ptr = strtok(NULL, "/");
-    
-    char *new_path = malloc(sizeof(char) * strlen(path));
+    navigation_data_t *navigation_data = (navigation_data_t *)malloc(sizeof(navigation_data_t));
+    navigation_data->curr_node = root;
+    navigation_data->root = root;
+    navigation_data->stack_path = stack_init("/");
 
-    while (ptr)
+    char *opt = (char *)malloc(sizeof(char) * 100);
+    do
     {
-        strcat(new_path, ptr);
-        strcat(new_path, "/");
-        ptr = strtok(NULL, "/");
-    }
+        printf("sudo@root:%s $ ", fs_get_current_path(navigation_data));
+        scanf(" %[^\n]s", opt);
+        char *cmd = strtok(opt, " ");
+        char *arg = strtok(NULL, " ");
 
-    return new_path;
+        if (strcmp(cmd, "ls") == 0)
+            cmd_list(navigation_data->curr_node);
+        else if (strcmp(cmd, "cd") == 0)
+            cmd_change_directory(&navigation_data, arg);
+        else if (strcmp(cmd, "help") == 0)
+            printf("cd clear exit help ls mkdir nano rm");
+        else if (strcmp(cmd, "mkdir") == 0)
+            tree_create_node(navigation_data->curr_node, arg, _FOLDER, true, true);
+        else if (strcmp(cmd, "nano") == 0)
+            tree_create_node(navigation_data->curr_node, arg, _FILE, true, true);
+        else if (strcmp(cmd, "exit") == 0)
+            break;
+        else if (strcmp(cmd, "clear") == 0)
+            system("clear");
+        else
+            printf("Type \"help\" for a list of commands.");
+
+        printf("\n");
+    } while (true);
+
+    return printf("\n");
 }
 
-void print_children(tree_node_t *node)
+char *find_current_path(navigation_data_t *navigation_data)
+{
+    char *path = (char *)malloc(sizeof(char) * 100);
+    tree_node_t *tmp = navigation_data->root;
+
+    while (tmp != navigation_data->curr_node)
+    {
+        strcat(path, tmp->data.name);
+        strcat(path, "/");
+        tmp = tmp->parent;
+    }
+
+    return path;
+}
+
+char *fs_get_current_path(navigation_data_t *navigation_data)
+{
+    char *path = (char *)malloc(sizeof(char) * 100);
+    stack_t *token = navigation_data->stack_path;
+    stack_t *tmp = stack_init(token->data);
+
+    while (token)
+    {
+        token = token->next;
+        if (token)
+            stack_push(&tmp, token->data);
+    }
+
+    token = tmp;
+
+    while (tmp)
+    {
+        strcat(path, tmp->data);
+        if (tmp->next && strcmp(path, "/") != 0)
+            strcat(path, "/");
+        tmp = tmp->next;
+    }
+
+    stack_free(token);
+
+    return path;
+}
+
+void cmd_list(tree_node_t *node)
 {
     if (node == NULL)
         return;
@@ -61,11 +215,15 @@ void print_children(tree_node_t *node)
     }
 }
 
-tree_node_t *create_node(tree_node_t *parent, char *name, enum Types type)
+tree_node_t *tree_create_node(tree_node_t *parent, char *name, enum Types type, bool can_read, bool can_write)
 {
     tree_node_t *node = malloc(sizeof(tree_node_t));
-    node->data.name = name;
+    char *node_name = (char *)malloc(sizeof(char) * 100);
+    node->data.name = strcpy(node_name, name);
     node->data.type = type;
+    node->data.content = "";
+    node->data.can_read = can_read;
+    node->data.can_write = can_write;
     node->parent = parent;
     node->child = NULL;
     node->sibling = NULL;
@@ -80,12 +238,12 @@ tree_node_t *create_node(tree_node_t *parent, char *name, enum Types type)
         else
         {
             tree_node_t *sibling = parent->child;
-            
+
             while (sibling->sibling != NULL)
             {
                 sibling = sibling->sibling;
             }
-            
+
             sibling->sibling = node;
         }
     }
@@ -93,9 +251,9 @@ tree_node_t *create_node(tree_node_t *parent, char *name, enum Types type)
     return node;
 }
 
-tree_node_t *find_by_path(tree_node_t *current, char *path, int depth)
+tree_node_t *tree_find(tree_node_t *current, char *path)
 {
-    if (current == NULL)
+    if (current == NULL || path == NULL)
         return NULL;
 
     // Returns the folder above
@@ -120,93 +278,123 @@ tree_node_t *find_by_path(tree_node_t *current, char *path, int depth)
     // Checks if the path starts in root
     if (strncmp(path, "/", strlen("/")) == 0)
     {
-        current = find_by_path(current, "/", 0);
-        depth = 0;
+        current = tree_find(current, "/");
     }
 
-    char *current_name = strtok(path, "/");
-    char *new_path = get_new_path(path, depth);
-
-    if (strcmp(current_name, current->data.name) == 0 && !new_path)
+    if (strcmp(path, "") == 0 || strcmp(path, current->data.name) == 0)
         return current;
 
     tree_node_t *tmp = current->child;
 
     while (tmp != NULL)
     {
-        if (strcmp(tmp->data.name, current_name) == 0)
-        {
-            return find_by_path(tmp, new_path, depth - 1);
-        }
+        if (strcmp(tmp->data.name, path) == 0)
+            return tmp;
 
         tmp = tmp->sibling;
     }
 
+    return NULL;
+}
+
+void cmd_change_directory(navigation_data_t **navigation_data, char *path)
+{
+    tree_node_t *new_current = tree_find((*navigation_data)->curr_node, path);
+
+    if (new_current == NULL)
+    {
+        printf("Directory not found\n");
+        return;
+    }
+
+    if (new_current->data.type == _FILE)
+    {
+        printf("Not a directory\n");
+        return;
+    }
+
+    if ((*navigation_data)->curr_node->parent == new_current)
+        stack_pop(&(*navigation_data)->stack_path);
+    else if ((*navigation_data)->curr_node != new_current)
+        stack_push(&(*navigation_data)->stack_path, path);
+
+    (*navigation_data)->curr_node = new_current;
+}
+
+tree_node_t *fs_init()
+{
+    tree_node_t *root = tree_create_node(NULL, "root", _FOLDER, true, true);
+
+    tree_node_t *home = tree_create_node(root, "home", _FOLDER, true, true);
+    tree_node_t *etc = tree_create_node(root, "etc", _FOLDER, true, true);
+    tree_node_t *bin = tree_create_node(root, "bin", _FOLDER, true, true);
+    tree_node_t *usr = tree_create_node(root, "usr", _FOLDER, true, true);
+    tree_node_t *var = tree_create_node(root, "var", _FOLDER, true, true);
+
+    tree_create_node(home, "user1", _FOLDER, true, true);
+    tree_create_node(home, "user2", _FOLDER, true, true);
+    tree_create_node(home, "user3", _FOLDER, true, true);
+
+    tree_create_node(etc, "passwd", _FILE, true, true);
+    tree_create_node(etc, "shadow", _FILE, true, true);
+    tree_create_node(etc, "group", _FILE, true, true);
+
+    tree_create_node(bin, "ls", _FILE, true, true);
+    tree_create_node(bin, "cd", _FILE, true, true);
+    tree_create_node(bin, "mkdir", _FILE, true, true);
+
+    tree_create_node(usr, "local", _FOLDER, true, true);
+    tree_create_node(usr, "share", _FOLDER, true, true);
+
+    tree_create_node(var, "log", _FOLDER, true, true);
+    tree_create_node(var, "tmp", _FOLDER, true, true);
+
+    return root;
+}
+
+stack_t *stack_init(char *data)
+{
+    stack_t *stack = (stack_t *)malloc(sizeof(stack_t));
+    stack->data = data;
+    stack->next = NULL;
+    return stack;
+}
+
+void stack_push(stack_t **stack, char *data)
+{
+    stack_t *new_data = (stack_t *)malloc(sizeof(stack_t));
+    new_data->data = (char *)malloc(sizeof(char) * (strlen(data) + 1));
+    strcpy(new_data->data, data);
+
+    new_data->next = *stack;
+    *stack = new_data;
+}
+
+stack_t *stack_pop(stack_t **stack)
+{
+    if (!stack)
+        return NULL;
+
+    stack_t *tmp = *stack;
+    *stack = tmp->next;
     return tmp;
 }
 
-
-int main()
+void stack_free(stack_t *stack)
 {
-    tree_node_t *root = create_node(NULL, "root", _FOLDER);
-    // tree_node_t *current = root;
-
-    tree_node_t *home = create_node(root, "home", _FOLDER);
-    create_node(root, "etc", _FOLDER);
-    create_node(root, "bin", _FOLDER);
-    create_node(root, "usr", _FOLDER);
-
-    create_node(home, "user", _FOLDER);
-    create_node(home, "root", _FOLDER);
-
-    char a[] = "/home/user";
-    
-    printf("%s", find_by_path(root, a, 0)->data.name);
-
-    // char *opt = (char *)malloc(sizeof(char) * 100);
-    // do
-    // {
-    //     printf("sudo@root:~$ ");
-    //     scanf(" %[^\n]s", opt);
-    //     char *cmd = strtok(opt, " ");
-    //     char *arg = strtok(NULL, " ");
-
-    //     if (strcmp(cmd, "ls") == 0)
-    //     {
-    //         if (arg)
-    //         {
-    //             tree_node_t *node = find_by_path(current, arg);
-    //             print_children(node);
-    //         }
-    //         else
-    //         {
-    //             print_children(current);
-    //         }
-    //     }
-    //     else if (strcmp(cmd, "help") == 0)
-    //     {
-    //         printf("cd exit help ls mkdir nano rm");
-    //     }
-    //     else if (strcmp(cmd, "mkdir") == 0)
-    //     {
-    //         create_node(root, arg, _FOLDER);
-    //     }
-    //     else if (strcmp(cmd, "nano") == 0)
-    //     {
-    //         create_node(root, arg, _FILE);
-    //     }
-    //     else if (strcmp(cmd, "exit") == 0)
-    //     {
-    //         break;
-    //     }
-    //     else
-    //     {
-    //         printf("Type \"help\" for a list of commands.");
-    //     }
-
-    //     printf("\n");
-    // } while (true);
-    
-
-    // return printf("\n");
+    stack_t *top = stack;
+    while (top != NULL)
+    {
+        stack_t *next = top->next;
+        free(top);
+        top = next;
+    }
+    // free(stack);
 }
+
+tree_node_t *fs_create_folder(tree_node_t *parent, char *name)
+{
+
+}
+
+tree_node_t *fs_create_file(tree_node_t *parent, char *name, char *content, bool can_read, bool can_write);
